283b50336adb41e5e890ccb5f8784f6a87397030
Snu Seam <siamahme766@gmail.com>
2026-07-27 13:32:38 +0600

    post created

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import { postRouter } from "./modules/post/post.route";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/posts", postRouter);
 
 export default app;
