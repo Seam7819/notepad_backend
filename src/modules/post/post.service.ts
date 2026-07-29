e2a94c6d65d813fba3b16e3bd77bf8f6e58bc7f0
Snu Seam <siamahme766@gmail.com>
2026-07-29 12:50:48 +0600

    search

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -8,8 +8,15 @@ const createPost = async (data: Omit<Posts, "id" | "createdAt" | "updatedAt">) =
     return result;
 }
 
-const getAllPosts = async ()=>{
-    const result = await prisma.posts.findMany();
+const getAllPosts = async (payload:{search : string | undefined})=>{
+    const result = await prisma.posts.findMany({
+        where : {
+            title :{
+                contains : payload.search as string,
+                mode : "insensitive"
+            }
+        }
+    });
     return result;
 }
 
